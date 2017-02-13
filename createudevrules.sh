9f99885cdcef0390012b33a84d46a2e27481d4a9
Essarelle <essarelle@nym.hush.com>
2017-02-13 20:18:10 +0100

    Update createudevrules.sh

--- createudevrules.sh
+++ createudevrules.sh
@@ -2,4 +2,5 @@
 # create custom rules for udev for mbzirc hardware
 sudo cp config/99-fisheye.rules /etc/udev/rules.d/
 sudo cp config/99-perspective.rules /etc/udev/rules.d/
+sudo cp config/99-RTK_GPS.rules /etc/udev/rules.d/
 sudo udevadm control --reload-rules && udevadm trigger
