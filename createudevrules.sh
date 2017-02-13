e6459b2469f197223de9cdf17775fd98109dfc31
Essarelle <essarelle@nym.hush.com>
2017-02-13 20:18:38 +0100

    Update createudevrules.sh

--- createudevrules.sh
+++ createudevrules.sh
@@ -3,4 +3,6 @@
 sudo cp config/99-fisheye.rules /etc/udev/rules.d/
 sudo cp config/99-perspective.rules /etc/udev/rules.d/
 sudo cp config/99-RTK_GPS.rules /etc/udev/rules.d/
+
+# trigger changes to take effect
 sudo udevadm control --reload-rules && udevadm trigger
